e9782afafa2ae11c844b25663f182d65c7f042aa
serllet <sfuentes18@alumnos.utalca.cl>
2023-10-04 23:51:39 -0300

    idea AFND

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import networkx as nx
 
 def validar_expresion_regular(expresion):
     try:
@@ -7,9 +8,45 @@ def validar_expresion_regular(expresion):
     except re.error:
         return False
 
-expresion = input("Ingrese una expresión regular (con a-z, A-Z, 0-9, ., |, *, (, ), ε, ∼, Φ): ")
+def construir_AFND(expresion):
+    grafo = nx.DiGraph()
+    pila = []
 
-if validar_expresion_regular(expresion):
-    print("La expresión regular es válida.")
-else:
-    print("La expresión regular no es válida.")
+    for caracter in expresion:
+        if caracter == '(':
+            pila.append(grafo.copy())
+            grafo = nx.DiGraph()
+        elif caracter == ')':
+            subgrafo = grafo
+            grafo = pila.pop()
+            grafo = nx.compose(grafo, subgrafo)
+        elif caracter == '|':
+            subgrafo = pila.pop()
+            pila.append(grafo.copy())
+            grafo = subgrafo
+        elif caracter == '*':
+            subgrafo = grafo.copy()
+            grafo.add_edge(0, 1, label='ε')
+            grafo.add_edge(1, 0, label='ε')
+            grafo.add_edge(0, 2, label='ε')
+            grafo.add_edge(2, 1, label='ε')
+            grafo.add_edge(2, 3, label='ε')
+            grafo.add_edge(3, 0, label='ε')
+            grafo = nx.compose(grafo, subgrafo)
+        else:
+            grafo.add_edge(0, 1, label=caracter)
+
+    grafo.add_node(2, accepting=True)  # Nodo de aceptación
+
+    return grafo
+
+def main():
+    expresion = input("Ingrese una expresión regular (con a-z, A-Z, 0-9, ., |, *, (, ), ε, ∼, Φ): ")
+
+    if validar_expresion_regular(expresion):
+        #print("La expresión regular es válida.")
+        print(construir_AFND(expresion))
+    else:
+        print("La expresión regular no es válida.")
+main()
+
