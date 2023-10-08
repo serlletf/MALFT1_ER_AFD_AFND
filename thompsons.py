db054a85f9a2d0931caed143cfc034f60f27ab0e
Benjamin Gonzalez <begonzalez18@alumnos.utalca.cl>
2023-10-08 18:04:55 -0300

    Intento de implementacion de thompsons
    Se crea un idea de como podrian generarse los thompson.
    Falta cambiar la estructura de datos para mejorar la inserción de nodos.

--- thompsons.py
+++ thompsons.py
@@ -1,76 +1,111 @@
 import nodos 
 
+nUltimoNodo = 0
+
+def aumenarNUltimoNodo():
+    global nUltimoNodo
+    nUltimoNodo += 1
+    return nUltimoNodo
+
 def thompsonChar(caracter):
-    inicio = "q0"
-    final = "q1"
-    return [inicio, caracter, final]
+    inicio = "q" + str(nUltimoNodo)
+    final = "q" + str(aumenarNUltimoNodo())
+    return [[inicio, caracter, final]]
     
 def thompsonVacio():
-    inicio = "q0"
-    final = "q2"
+    inicio = "q" + str(nUltimoNodo)
+    final = "q" + str(aumenarNUltimoNodo())
     return [inicio, "_", final]
 
 def thompsonConcatenacion(th1, th2):
-    nodos = [["q0", th1, "q1"],["q1", "_", "q2"], ["q2", th2, "q2"]]
+    q0 = "q" + str(nUltimoNodo)
+    q1 = "q" + str(aumenarNUltimoNodo())
+    q2 = "q" + str(aumenarNUltimoNodo())
+    q3 = "q" + str(aumenarNUltimoNodo())
+    
+    nodos = [[q0, th1, q1],[q1, "_", q2], [q2, th2, q3]]
     return nodos
 
 def thompsonO(th1, th2):
-    inicio = th1[0]
-    final = th2[th2.size() - 1]
-    return [inicio, "_", th2]
+    q0 = "q" + str(nUltimoNodo)
+    q1 = th1[0]
+    q2 = th1[len(th1)-1]
+    q3 = th2[0]
+    q4 = th2[len(th2)-1]
+    q5 = "q" + str(aumenarNUltimoNodo())
+    return [[q0, "_",q1],[q2,"_",q5],[q0, "_",q3],[q4,"_",q5]]
 
 def thompsonKleene(th):
-    inicio = "q0"
-    final = "q2"
-    nodos = [["q0", "_", "q1"],["q0", "_", "q3"], ["q1", th, "q2"], ["q2", "_", "q1"], ["q2", "_", "q3"]]
+    q0 = "q" + str(nUltimoNodo)
+    q1 = "q" + str(aumenarNUltimoNodo())
+    q2 = "q" + str(aumenarNUltimoNodo())
+    q3 = "q" + str(aumenarNUltimoNodo())
+    
+    nodos = [[q0, "_", q1],[q0, "_", q3], [q1, th, q2], [q2, "_", q1], [q2, "_", q3]]
     return nodos
 
 
-nodos = []
 def capturar_parentesis(expresion):
     grupo = []
-    for i in range(len(expresion) - 1 ):
+    nparentesisAbierto = 0
+    for i in range(len(expresion)):
         caracter = expresion[i]
         if(caracter== "("):
-            grupo.append(i)
-        elif(caracter== ")"):
-            grupo.append(i)
+            nparentesisAbierto += 1
+            if(nparentesisAbierto == 1):continue #Ignorar el primer"(" 
+        if(caracter== ")"):
+            nparentesisAbierto -= 1
+            if(nparentesisAbierto == 0):continue #Ignorar el ultimo"(" 
+        if(nparentesisAbierto > 0):
+            grupo.append(caracter)
     return grupo
 
 
+
+def obtenerTh2(expresion):
+    th2 = []
+    for caracter in expresion:
+        if(caracter== "("):
+            continue
+        if (not(caracter.isalpha() or caracter.isdigit())):
+            return th2
+        th2.append(caracter)
+    return th2
+
 def parsear(expresion):
+    nNodoActual = 0
     a = lambda i : expresion[i-1]
     b = lambda i : expresion[i+1]
     th1 = []
     th2 = []
     nuevaExpresion = []	
-    
-    for i in range(len(expresion) - 1 ):
+    for i in range(len(expresion)):
         caracter = expresion[i]
-        
         if(caracter== "("):
             nuevaExpresion = capturar_parentesis(expresion)
-            print(nuevaExpresion)
+            if th1== []:
+                th1 = nuevaExpresion #Aca se debe hacer una recursion en la nueva expresion
+                continue
+            th2 = nuevaExpresion
             continue
         if(caracter== ")"):
             continue
-        if(caracter == "."):
-            th1 = thompsonConcatenacion(a(i), b(i))
-            print(th1)
+        if(caracter=="."):
+            th2 = obtenerTh2(expresion[i+1:])
+            #th1 = thompsonConcatenacion(th1, th2)
             continue
         if(caracter == "|"):
-            th1 = thompsonO(th1, b(i))
-            print(th1)
+            #th1 = thompsonO(th1, b(i))
             continue
         if(caracter == "*"):
-            th1 = thompsonKleene(th1)
-            print(th1)
+            #th1 = thompsonKleene(th1)
             continue
         if(caracter == "_"):
-            th1 = thompsonVacio()
-            print(th1)
-        th1 =  caracter
-        return th1
+            #th1 = thompsonVacio()
+            continue
+        th1.append(thompsonChar(caracter))
+    return th1
+
 
 
 def toList(expresion):
@@ -80,8 +115,9 @@ def toList(expresion):
     return lista
 
 def main():
-    expresion = "a.b.c"
-    parsear(list(expresion))
+    expresion = "((a.b)|a).c)"
+    resultado = parsear(list(expresion))
+    print(resultado)
     
     
 main()
