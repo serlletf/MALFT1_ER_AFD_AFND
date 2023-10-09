0f16bc4ef8dfb00f06845848929f774ea2702063
Benjamin Gonzalez <begonzalez18@alumnos.utalca.cl>
2023-10-09 13:12:53 -0300

    Se procesa recursivamente de izquierda a derecha
    Pendiente:
    -Queda cambiar la estructura de datos para mejorar la inserción.
    -Comprobar funciones Thompsons
    -Añadir prioridad a los parentesis

--- thompsons.py
+++ thompsons.py
@@ -10,7 +10,7 @@ def aumenarNUltimoNodo():
 def thompsonChar(caracter):
     inicio = "q" + str(nUltimoNodo)
     final = "q" + str(aumenarNUltimoNodo())
-    return [[inicio, caracter, final]]
+    return [inicio, caracter, final]
     
 def thompsonVacio():
     inicio = "q" + str(nUltimoNodo)
@@ -23,7 +23,7 @@ def thompsonConcatenacion(th1, th2):
     q2 = "q" + str(aumenarNUltimoNodo())
     q3 = "q" + str(aumenarNUltimoNodo())
     
-    nodos = [[q0, th1, q1],[q1, "_", q2], [q2, th2, q3]]
+    nodos = [q0, th1, q1],[q1, "_", q2],[q2, th2, q3]
     return nodos
 
 def thompsonO(th1, th2):
@@ -41,7 +41,7 @@ def thompsonKleene(th):
     q2 = "q" + str(aumenarNUltimoNodo())
     q3 = "q" + str(aumenarNUltimoNodo())
     
-    nodos = [[q0, "_", q1],[q0, "_", q3], [q1, th, q2], [q2, "_", q1], [q2, "_", q3]]
+    nodos = [q0, "_", q1,[q0, "_", q3], [q1, th, q2], [q2, "_", q1], [q2, "_", q3]]
     return nodos
 
 
@@ -60,8 +60,6 @@ def capturar_parentesis(expresion):
             grupo.append(caracter)
     return grupo
 
-
-
 def obtenerTh2(expresion):
     th2 = []
     for caracter in expresion:
@@ -72,58 +70,49 @@ def obtenerTh2(expresion):
         th2.append(caracter)
     return th2
 
+th1 = []
+operacion = ''
+
+def operar(caracter):
+    global th1,operacion
+    if (operacion == '.'):
+        th1 = thompsonConcatenacion(th1, caracter)
+    if (operacion == '|'):
+        th1 = thompsonO(th1, caracter)
+    if (operacion == '*'):
+        th1 = thompsonKleene(th1)  
+    if (operacion == '_'):
+        th1 = thompsonVacio()
+    operacion = ''    
+    return
+
+
 def parsear(expresion):
-    nNodoActual = 0
-    a = lambda i : expresion[i-1]
-    b = lambda i : expresion[i+1]
-    th1 = []
-    th2 = []
-    nuevaExpresion = []	
-    for i in range(len(expresion)):
-        caracter = expresion[i]
-        if(caracter== "("):
-            nuevaExpresion = capturar_parentesis(expresion)
-            if th1== []:
-                print("Nueva e: " ,nuevaExpresion)
-
-                th1 = parsear(nuevaExpresion)
-                print("TH1: " ,th1)
-                continue
-            th2 = nuevaExpresion
-            continue
-        if(caracter== ")"):
-            continue
-        if(caracter=="."):
-            th2 = obtenerTh2(expresion[i+1:])
-            print("TH2: " ,th2)
-            #th1 = thompsonConcatenacion(th1, th2)
-            continue
-        if(caracter == "|"):
-            #th1 = thompsonO(th1, b(i))
-            continue
-        if(caracter == "*"):
-            #th1 = thompsonKleene(th1)
-            continue
-        if(caracter == "_"):
-            #th1 = thompsonVacio()
-            continue
-        th1.append(thompsonChar(caracter))
-    return th1
-
-def crearOrdenOperaciones(expresion):
-    for i in range(len(expresion)):
-        caracter = expresion[i]
-        if(caracter== "("):
-            nuevaExpresion = capturar_parentesis(expresion)
-            if th1== []:
-                th1 = nuevaExpresion #Aca se debe hacer una recursion en la nueva expresion
-                continue
-            th2 = nuevaExpresion
-            continue
-        if(caracter== ")"):
-            continue
+    global th1,operacion
+    print("Asi va ",expresion, "Largo ", len(expresion))
     
-
+    if(len(expresion) == 0):
+        return 
+    
+    parsear(expresion[:-1])
+    print("De vuelta para procesar de izquierda a derecha ", expresion)
+    
+    caracter = expresion[len(expresion) - 1]
+    
+    #Si hay alguna operacion pendiente, se realiza con el thompson ya existente y el caracter actualmente por procesar
+    if(operacion != ''):
+        operar(caracter)
+        return
+        
+    #Si se procesa una letra o un numero se hace un thompson de un caracter    
+    if(caracter.isalpha() or caracter.isdigit()):
+        th1 = thompsonChar(caracter)
+        return 
+    
+    #Como no es un caracter ni un numero, se asume que es una operacion y se agrega como operacion pendiente
+    operacion = caracter
+    return
+     
 
 
 def toList(expresion):
@@ -133,9 +122,10 @@ def toList(expresion):
     return lista
 
 def main():
-    expresion = "((a.b)|a).c)"
+    expresion = "a.b|c"
     resultado = parsear(list(expresion))
     print(resultado)
+    print(th1)
     
     
 main()
