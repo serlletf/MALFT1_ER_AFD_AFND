7146ab481a5d8b9ad8b7dfb3d1a8354da7258d5a
Mrldmz <49105425+Mrldmz@users.noreply.github.com>
2023-10-14 14:44:08 -0300

    reestructuracion de clases Nodo y Th

--- estructura.py
+++ estructura.py
@@ -1,12 +1,17 @@
+nodoCount = 0
+
+#las transiciones de los nodos se guardan en formato de listas
+#ej: nodo1.transicion = [["_", nodo2], ["a", nodo3]]
 class Nodo:
-    def __init__(self ):
+    def __init__(self, id):
         self.transicion = []
-        
-        
+        self.id = id
+
+
 class Th:
     def __init__(self):
         self.inicio = 0
-        self.final  = 0
+        self.final = 0
         self.transiciones = []
         
     def agregarTransicion(self,nodo):
@@ -14,4 +19,64 @@ class Th:
         
     def agregarInicioTransicion(self,nodo):
         self.transiciones.insert(0,nodo)
-        
+
+    #retorna un nuevo thompson
+    def th_o(self, th_sgte):
+        nuevoTh = Th()
+        global nodoCount
+
+        #nuevos nodos
+        nuevoTh.inicio = Nodo(nodoCount)
+        nodoCount += 1
+
+        nuevoTh.final = Nodo(nodoCount)
+        nodoCount += 1
+
+        #transiciones de los nodos
+
+        #transiciones del nuevo inicio
+        nuevoTh.inicio.transicion.append("_", self.inicio)
+        nuevoTh.inicio.transicion.append(["_", th_sgte.inicio])
+
+        #transiciones hacia el nuevo final
+        self.final.transicion.append(["_", nuevoTh.final])
+        th_sgte.final.transicion.append("_", nuevoTh.final)
+
+        return nuevoTh
+
+    def th_concat(self, th_sgte):
+        nuevoTh = Th()
+        #concatenacion no crea nodos, solo transiciones
+
+        #nueva transicion epsilon
+        self.final = th_sgte.inicio
+        self.final.transicion.append(["_", th_sgte.inicio])
+
+        #inicio y final del nuevo thompson
+        nuevoTh.inicio = self.inicio
+        nuevoTh.final = th_sgte.final
+
+        return nuevoTh
+
+    def th_kleene(self):
+        nuevoTh = Th()
+        global nodoCount
+
+        #nuevos nodos
+        nuevoTh.inicio = Nodo(nodoCount)
+        nodoCount += 1
+
+        nuevoTh.final = Nodo(nodoCount)
+        nodoCount += 1
+
+        #transiciones de los nodos
+
+        #transiciones del nuevo inicio
+        nuevoTh.inicio.transicion.append(["_", self.inicio])
+        nuevoTh.inicio.transicion.append(["_", nuevoTh.final])
+
+        #transiciones del viejo final
+        self.final.transicion.append(["_", self.inicio])
+        self.final.transicion.append(["_", nuevoTh.final])
+
+        return nuevoTh
