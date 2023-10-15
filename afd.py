1c19b9f4f080201715690d7a871117ac36fe83e4
BenjaminGonzalezF <begonzalez18@alumnos.utalca.cl>
2023-10-15 11:49:28 -0300

    Generar un nodo de nuevos estados
    A partir de un nodo inicio, calcula sus transiciones vacias o si esta conectado con el caracter a buscar.

--- afd.py
+++ afd.py
@@ -3,13 +3,14 @@ from estructura import Th,Nodo
 
 class AFD:
     afnd = Th()
-    
+    nuevosEstados = []
+
     def __init__(self, afnd):
         self.afnd = afnd
 
     def AFNDToAFD(self):
         self.armarUniones()
-        self.crearTabla()
+        self.crearTabla()        
         return
     
     def armarUniones(self):
@@ -33,30 +34,85 @@ class AFD:
                     #print("Transicion vacia con ", nodoUnido["nombre"])
                     nodo.transicionesVacias.append(nodoUnido["nombre"])
                     #self.buscarTransicionesVacias(self.afnd.retornarNodo(nodoUnido["nombre"]))
-          
+                    
+    def buscarCaracterEnTransicionesVacias(self,uniones,caracter):
+        self.nuevosEstados
+        # union es un diccionario en este formato{"nombre": destino, "valor" :etiqueta}
+        for union in uniones:
+            if(union["valor"] == "_" or union["valor"] == caracter):
+                if(union["nombre"] not in self.nuevosEstados):
+                    self.nuevosEstados.append(union["nombre"])
+                    nodoARecorrer = self.afnd.retornarNodo(union["nombre"])
+                    unionesNuevas = nodoARecorrer.uniones
+                    if(unionesNuevas != []):
+                        print("Se hace recursion con ", nodoARecorrer.nombre, " y sus uniones ", unionesNuevas)
+                        self.buscarCaracterEnTransicionesVacias(unionesNuevas,caracter)
+        return
+         
     def crearTabla(self):
         tabla = []
         nodos = self.afnd.nodos
         alfabeto = self.afnd.alfabeto
+        
+        #Lista de caracteres del alfabeto
         caracteres = []
-
+        
+        #Lista de estados del AFD
         nodosAfd = []
+        #nodos que en la iteracion son vacios, esta lista es auxiliar para luego ser agregada en las demás
         estados = []
+        #Nodo inicial
         inicio =  self.afnd.retornarNodo("q" + str(self.afnd.inicio))
         final = self.afnd.final
+        
+        #Corresponde a la lista que contine los nodo que se pueden llegar con transiciones vacia
+        # ej: Alfabeto: [a,b] 
+        # delta        | a | b |
+        # nodos afd    | [[0.estadosAlfabeto] | ,[0.estadosAlfabeto]] |
+        # nodos afd    | [[0.estadosAlfabeto],[1.estadosAlfabeto[]] | ,[0.estadosAlfabeto],[1.estadosAlfabeto]] |
         estadosAlfabeto = []
+        
+        
+        #Creacion de la lista de caracteres del alfabeto y el tamaño de listas que habrán en estadosAlfabeto
+        #La cantidad de listas es igual a la cantidad de caracteres del alfabeto
         for caracter in alfabeto:
             caracteres.append(caracter)
             estadosAlfabeto.append([])
         
+        #Define el primer nodo al AFD que corresponde al inicio y sus transiciones vacías
         estados.append(inicio.nombre)
         for nodosTV in inicio.transicionesVacias:
             estados.append(nodosTV)
-        nodosAfd.append(estados)    
-          
-        print(caracteres)                  
-        print(estados)
-        print(nodosAfd)
+        print("EStados ",estados)
+        nodosAfd.append(estados.copy())
+        print("Nodos AFD ",nodosAfd)
+        print("Alfabeto", caracteres)    
+        
+        print("---------------------")
+        estados.clear()
+        nodo = self.afnd.retornarNodo("q" + str(self.afnd.inicio))
+        
+        self.buscarCaracterEnTransicionesVacias(nodo.uniones,"a")
+        print("Nuevos estados ",self.nuevosEstados)
+    
+        """ for caracter in caracteres:
+            #print("Caracter: ", caracter)
+            for nodoAfd in nodosAfd:
+                #print("Nodo AFD ", nodoAfd)
+                for nombreNodo in nodoAfd:
+                    nodo = Nodo()
+                    nodo = self.afnd.retornarNodo(nombreNodo)
+                
+                    print("Buscando uniones con el caracter: ", caracter , "en el nodo: ",nodo.nombre)
+                    for union in nodo.uniones:
+                        if(union["valor"] == caracter):
+                            estados.append(union["nombre"])
+                            print("Se preguntó los estados de ",nodo.nombre, " Se encontro una union con", union["nombre"], " Con el caracter ", caracter)
+                            break  """ 
+            
+        
+
+                                
         
         
         
